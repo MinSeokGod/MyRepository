44cc5244afe013fcd6b04cf2dbfa8af988b4d6a4
MinSeokGod <spkkang2@gmail.com>
2024-12-05 13:50:24 +0900

    First commit in dev branch

--- MyGitTest/src/mygit/YourClass.java
+++ MyGitTest/src/mygit/YourClass.java
@@ -1,5 +1,8 @@
 package mygit;
 
 public class YourClass {
-
+	public void sayHello(String toWhom) {
+		System.out.println("Hello! " + toWhom);
+		System.out.println("Your change in main branch");
+	}
 }
